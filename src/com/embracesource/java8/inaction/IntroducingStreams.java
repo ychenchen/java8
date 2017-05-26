b7da6be2f591fb045ef74a83c8647aa33235d25a
ychenchen <whutycc@163.com>
2017-05-26 21:57:31 +0800

    java8 in action-Introducing Streams

--- src/com/embracesource/java8/inaction/IntroducingStreams.java
+++ src/com/embracesource/java8/inaction/IntroducingStreams.java
@@ -139,9 +139,58 @@ public class IntroducingStreams {
 	}
 	
 	//4.3. Streams vs. collections
+	//A Collection in Java8 is like a movie stored in DVD; A Stream in Java8 is like a movie streamed over the Internet.
 	
+	//4.3.1. Traversable only once
+	//Note that, similarly to iterators, a stream can be traversed only once. After that a stream is said
+	//to be consumed. You can get a new stream from the initial data source to traverse it again just
+	//like for an iterator.
+	//So keep in mind that you can consume a stream only once!
+	//Another key difference between collections and streams is how they manage the iteration over data.
 	
+	//4.3.2. External vs. internal iteration
+	//Using the Collection interface requires iteration to be done by the user (for example, using
+	//for-each); this is called external iteration. The Streams library by contrast uses internal
+	//iteration—it does the iteration for you and takes care of storing the resulting stream value
+	//somewhere; you merely provide a function saying what’s to be done. 
 	
+	//using an internal iteration,
+	//the processing of items could be transparently done in parallel or in a different order that may
+	//be more optimized. These optimizations are difficult if you iterate the collection externally as
+	//you’re used to doing in Java. 
+	
+	//4.4. Stream operations
+	//The Stream interface in java.util.stream.Stream defines many operations. They can be classified into two categories.
+	//Stream operations that can be connected are called intermediate operations, and operations
+	//that close a stream are called terminal operations. 
+	
+	//4.4.1. Intermediate operations
+	//Intermediate operations such as filter or sorted return another stream as the return type. This
+	//allows the operations to be connected to form a query. What’s important is that intermediate
+	//operations don’t perform any processing until a terminal operation is invoked on the stream
+	//pipeline—they’re lazy.
+	
+	//4.4.2. Terminal operations
+	//Terminal operations produce a result from a stream pipeline. A result is any nonstream value
+	//such as a List, an Integer, or even void. 
+	
+	//4.4.3. Working with streams
+	//To summarize, working with streams in general involves three items:
+	//A data source (such as a collection) to perform a query on
+	//A chain of intermediate operations that form a stream pipeline
+	//A terminal operation that executes the stream pipeline and produces a result
+	
+	//4.5. Summary
+	//Here are some key concepts to take away from this chapter:
+	//A stream is a sequence of elements from a source that supports data processing operations.
+	//Streams make use of internal iteration: the iteration is abstracted away through operations such as
+	//filter, map, and sorted.
+	//There are two types of stream operations: intermediate and terminal operations.
+	//Intermediate operations such as filter and map return a stream and can be chained together. They’re
+	//used to set up a pipeline of operations but don’t produce any result.
+	//Terminal operations such as forEach and count return a nonstream value and process a stream
+	//pipeline to return a result.
+	//The elements of a stream are computed on demand
 	public static void main(String[] args) {
 
 	}
